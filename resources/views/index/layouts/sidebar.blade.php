daab8e4f9fe5dafe4ab9af86a2a2af78b7e9e561
hoangtu <hoangtu@ohlaalaa.com>
2023-03-08 08:14:14 +0700

    add

--- resources/views/index/layouts/sidebar.blade.php
+++ resources/views/index/layouts/sidebar.blade.php
@@ -1,23 +1,7 @@
 <div class="app-menu navbar-menu">
     <div class="navbar-brand-box">
         <!-- Dark Logo-->
-        <a href="admin/index" class="logo logo-dark">
-            <span class="logo-sm">
-                <img src="{{ URL::asset('assets/images/logo-sm.png') }}" alt="" height="22">
-            </span>
-            <span class="logo-lg">
-                <img src="{{ URL::asset('assets/images/logo-dark.png') }}" alt="" height="17">
-            </span>
-        </a>
-        <!-- Light Logo-->
-        <a href="admin/index" class="logo logo-light">
-            <span class="logo-sm">
-                <img src="{{ URL::asset('assets/images/logo-sm.png') }}" alt="" height="22">
-            </span>
-            <span class="logo-lg">
-                <img src="{{ URL::asset('assets/images/logo-light.png') }}" alt="" height="17">
-            </span>
-        </a>
+       <div class="text-white fs-20">OHLAALAA</div>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
             <i class="ri-record-circle-line"></i>
         </button>
